e015ef96518d280917dd67f81f07e602f572c711
surume <sururne@gmail.com>
2020-08-31 18:58:07 +0900

    tmp

--- src/aws/dynamodb.cr
+++ src/aws/dynamodb.cr
@@ -3,6 +3,7 @@ require "./dynamodb/**"
 module AWS::DynamoDB
   # Represents the supported AWS DynamoDB operations (note: this is not exhaustive).
   enum Operation
+    BatchGetItem
     GetItem
     PutItem
     DeleteItem
